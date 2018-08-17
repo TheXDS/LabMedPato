ec975598f8c71cda8f865c27c4ef56bba2c3d03e
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2018-08-17 16:35:01 -0600

    Identificación de versión

--- FormRender/MainWindow.xaml.cs
+++ FormRender/MainWindow.xaml.cs
@@ -4,9 +4,11 @@ using FormRender.Pages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Interop;
 using TheXDS.MCART;
 using static TheXDS.MCART.Networking.DownloadHelper;
 
@@ -17,11 +19,62 @@ namespace FormRender
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const uint WS_EX_CONTEXTHELP = 0x00000400;
+        private const uint WS_MINIMIZEBOX = 0x00020000;
+        private const uint WS_MAXIMIZEBOX = 0x00010000;
+        private const int GWL_STYLE = -16;
+        private const int GWL_EXSTYLE = -20;
+        private const int SWP_NOSIZE = 0x0001;
+        private const int SWP_NOMOVE = 0x0002;
+        private const int SWP_NOZORDER = 0x0004;
+        private const int SWP_FRAMECHANGED = 0x0020;
+        private const int WM_SYSCOMMAND = 0x0112;
+        private const int SC_CONTEXTHELP = 0xF180;
+
+
+        [DllImport("user32.dll")]
+        private static extern uint GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hwnd, int index, uint newStyle);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPos(IntPtr hwnd, IntPtr hwndInsertAfter, int x, int y, int width, int height, uint flags);
         private class ApiInfo
         {
             internal string ruta;
             internal Language language;
         }
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var hwnd = new WindowInteropHelper(this).Handle;
+            var styles = GetWindowLong(hwnd, GWL_STYLE);
+            styles &= 0xFFFFFFFF ^ (WS_MINIMIZEBOX | WS_MAXIMIZEBOX);
+            SetWindowLong(hwnd, GWL_STYLE, styles);
+            styles = GetWindowLong(hwnd, GWL_EXSTYLE);
+            styles |= WS_EX_CONTEXTHELP;
+            SetWindowLong(hwnd, GWL_EXSTYLE, styles);
+            SetWindowPos(hwnd, IntPtr.Zero, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+            ((HwndSource)PresentationSource.FromVisual(this)).AddHook(HelpHook);
+        }
+
+        private IntPtr HelpHook(IntPtr hwnd,
+            int msg,
+            IntPtr wParam,
+            IntPtr lParam,
+            ref bool handled)
+        {
+            if (msg == WM_SYSCOMMAND &&
+                ((int)wParam & 0xFFF0) == SC_CONTEXTHELP)
+            {
+                var asm = GetType().Assembly;
+                MessageBox.Show($"{asm.GetName().Name} v{asm.GetName().Version}");
+                handled = true;
+            }
+            return IntPtr.Zero;
+        }
+
 
         bool queueClose = true;
         string usr = string.Empty, pw = string.Empty;
